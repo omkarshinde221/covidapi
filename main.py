5d824aa3c05e41eabefbb97a8fdd96be4c7c0a82
omkarshinde221 <rock.omkar22@gmail.com>
2022-06-21 14:36:30 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, request, render_template
+import requests
+import os
+import matplotlib.pyplot as plt
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    text = request.args.get('country')
+    # countryName = data['country']
+    corona_data = 'https://disease.sh/v3/covid-19/countries/' + text
+    data = requests.get(corona_data).json()
+    allData = {'cases': data['cases'], 'death': data['deaths'], 'recovered': data['recovered'],
+               'active': data['active']}
+
+    opData = list(allData.keys())
+    values = list(allData.values())
+
+    fig = plt.figure()
+
+    plt.pie(values, labels=opData, autopct='%.2f')
+
+    plt.title(text)
+    fig.savefig('static/test.png')
+    plt.close(fig)
+    image_data = os.path.join('/static/', 'test.png')
+
+
+    return render_template("index.html", image=image_data)
+
+if __name__ == "__main__":
+    app.run()
+
